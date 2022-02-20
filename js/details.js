5f1c4a7dcb353368480c3716331484f42ef2b5d5
rune59 <73398277+rune59@users.noreply.github.com>
2022-02-20 19:38:49 +0100

    Initial commit

---
+++ js/details.js
@@ -0,0 +1,56 @@
+
+const detailContainer = document.querySelector(".oneProduct");
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+console.log(id);
+
+// This link is for developing on my local computer
+// const url = "http://localhost:10018/wp-json/wc/store/products/" + id;
+
+// And this link is for use after the WP and database has been uploaded 
+// from local server on my computer to my web-hotel. 
+const url = "http://runrun.no/rainydays/wp-json/wc/store/products/" + id;
+
+console.log(url);
+
+async function getThisProduct() {
+
+    try {
+        const response = await fetch(url);
+        const thisProduct = await response.json();
+        // console.log("Name:" + thisProduct.name);
+        // console.log("Price:" + thisProduct.prices.price);
+        // console.log("Type:" + thisProduct.type);
+        // console.log("Short description:" + thisProduct.short_description);
+        // console.log("Category:" + thisProduct.categories[0].name);
+        // console.log("Quantity in stock:" + thisProduct.quantity_limit);
+
+        createHTML(thisProduct);
+    }
+    catch(error) {
+        console.log(error);
+    }
+    
+}
+getThisProduct();
+
+
+function createHTML(thisProduct) {
+    detailContainer.innerHTML =
+            `<div class="oneProduct">
+                <h2>${thisProduct.name}</h2>
+                <img src="${thisProduct.images[0].src}" alt="${thisProduct.name}">
+                <h3>Product name: ${thisProduct.name}</h3>
+                <h3>Price in NOK: ${thisProduct.prices.price}</h3>
+                <h3>Short description: ${thisProduct.short_description}</h3>
+                <h3>Category: ${thisProduct.categories[0].name}</h3>
+                <h3>Type: ${thisProduct.type}</h3>
+                <h3>Quantity in stock: ${thisProduct.quantity_limit}</h3>
+            </div>`
+        ;
+}
+
+
+
---
+++ js/script.js
@@ -0,0 +1,71 @@
+
+// This link is for developing on my local computer
+// const baseUrl = "http://localhost:10018/wp-json/wc/store/products";
+
+// And this link is for use after the WP and database has been uploaded 
+// from local server on my computer to my web-hotel. 
+const baseUrl = "http://runrun.no/rainydays/wp-json/wc/store/products";
+
+
+const productContainer = document.querySelector(".manyProducts");
+const perPage = document.querySelector(".per-page-selection");
+const categories = document.querySelectorAll(".categories");
+const searchButton = document.querySelector(".search-button");
+
+async function getProducts(url) {
+    try {
+        const response = await fetch(url);
+        const getResults = await response.json();
+        productContainer.innerHTML = "";
+        createHTML(getResults);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+getProducts(baseUrl);
+
+function createHTML(products) {
+    products.forEach(function (product) {
+        productContainer.innerHTML +=
+            `<a href="details.html?id=${product.id}">
+            <div class="oneProduct">
+                <h2>${product.name}</h2>
+                <img src="${product.images[0].src}" alt="${product.name}">
+            </div>
+            </a>`
+            
+    });
+}
+
+perPage.onchange = function (event) {
+    const newUrl = baseUrl + `?per_page=${event.target.value}`;
+    productContainer.innerHTML = "";
+    getProducts(newUrl);
+}
+
+categories.forEach(function (category) {
+    category.onclick = function (event) {
+        let newUrl;
+        if (event.target.id === "featured") {
+            newUrl = baseUrl + "?featured=true";
+        }
+        else {
+            const categoryChosen = event.target.value;
+            // newUrl = this.baseUrl + `?category=${categoryChosen}`
+            newUrl = baseUrl + `?category=${categoryChosen}`
+        }
+        productContainer.innerHTML = "";
+        getProducts(newUrl);
+    }
+})
+
+// This part of the code does not work
+
+// searchButton.onclick = function () {
+//     const searchInput = document.querySelector("#search-input");
+//     console.log(searchInput);
+//     const newUrl = baseUrl + `?search=${searchInput}`;
+//     productContainer.innerHTML = "";
+//     getProducts(newUrl);
+// }
